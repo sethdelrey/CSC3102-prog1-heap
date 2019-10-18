d7b607bac166872843ba868a507ed42929ca301a
rhodgeson <55752201+rhodgeson@users.noreply.github.com>
2019-10-17 22:31:31 -0500

    cleaned up minheap

--- src/MinHeap.java
+++ src/MinHeap.java
@@ -1,11 +1,11 @@
 public class MinHeap {
-    private int[] Heap;
-    private int size;
-    private int maxsize;
-    private int k;
+    private int[] Heap;     //array representation of the minHeap
+    private int size;       //number of current elements in the heap
+    private int maxsize;    //maximum number of elements the heap can hold
+    private int k;          //maximum number of children each parent element can hold
 
-    private static final int FRONT = 0;
-
+    //constructor for k-ary MinHeap with given maxsize
+    //and given k
     public MinHeap(int maxsize, int k) {
         this.maxsize = maxsize;
         this.size = 0;
@@ -13,11 +13,12 @@ public class MinHeap {
         this.k = k;
     }
 
+    //returns the parent element of the element in the given index pos
     private int parent(int pos) {
         return pos/k;
     }
 
-
+    //returns true if the element at index pos has no children
     private boolean isLeaf(int pos) {
         if (pos >= (size/k) && pos <= size) {
             return true;
@@ -25,7 +26,7 @@ public class MinHeap {
         return false;
     }
 
-    //swaps 2 nodes of the heap
+    //swaps 2 nodes of the heap at the indices fpos and spos
     private void swap(int fpos, int spos) {
         int temp;
         temp = Heap[fpos];
@@ -33,6 +34,8 @@ public class MinHeap {
         Heap[spos] = temp;
     }
 
+    //Method to restore the heap property of the nodes
+    //of the subtree rooted at index pos
     private void minHeapify(int pos) {
         //if node is nonleaf & greater than
         //any of its children
@@ -69,6 +72,8 @@ public class MinHeap {
         }
     }
 
+    //Method to insert a value in the heap
+    //Parameter x is the key of the element
     public void insert(int x) {
         if (size >= maxsize) {
             return;
@@ -82,11 +87,13 @@ public class MinHeap {
        }
     }
 
+    //Method that returns the key of the minimum value
+    //of the heap and restores the heap property
+    //of the remaining nodes
     public int extractMin() {
-        int popped = Heap[FRONT];
-        Heap[FRONT] = Heap[--size];  
-        minHeapify(FRONT);
+        int popped = Heap[0];
+        Heap[0] = Heap[--size];
+        minHeapify(0);
         return popped;
     }
-
 }
